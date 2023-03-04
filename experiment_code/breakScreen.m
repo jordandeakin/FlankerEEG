2e42352bbb6f7cebc9de919f06c1edc18b9614fd
Jordan Deakin (PhD Psychology Lab FT) <JXD460@student.bham.ac.uk>
2023-03-04 18:21:45 +0000

    Last minute prettiness and auditory feedback

--- experiment_code/breakScreen.m
+++ experiment_code/breakScreen.m
@@ -1,10 +1,10 @@
 function breakScreen(w)
 RestrictKeysForKbCheck([KbName('space')])
-DrawFormattedText(w.ptr,'Time for a break!\n\nNot tired yet?','center','center',[255 255 255],100,[],[],2);
-DrawFormattedText(w.ptr,'Press space to skip!','center',w.Yrect+300,[0 255 255],100,[],[],2);%'baseColor',[255 255 255]);
+   DrawFormattedText2('<color=00FFFF><b>Time for a break!<color><b>\n\nNot tired yet?','win',w.ptr,'sx','center','sy','center','xalign','center','yalign','center','xlayout','center','baseColor',[255 255 255]);
+    DrawFormattedText(w.ptr,'Press space to skip!','center',w.Yrect+300,[0 255 255],100,[],[],2);%'baseColor',[255 255 255]);
 
 Screen('Flip',w.ptr)
 KbStrokeWait;
 
-RestrictKeysForKbCheck([KbName('z') KbName('m')])
+RestrictKeysForKbCheck([KbName('z') KbName('m') KbName('q')])
 end
